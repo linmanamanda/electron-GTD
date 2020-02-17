8a2c93edf496a4d9f80324cf9876b0c3b77d1b20
linman <linmanamanda@gmail.com>
2020-02-17 18:09:07 +0800

    doc: update README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# electron-GTD
+
+> 奇妙清单 + 番茄工作法
+
+## 项目功能
+- [] 利用坚果云同步
+
+## 项目截图
+
+## 技术栈
+1. electron
+2. Vue + Vue-Router + Vuex
+3. iView
+4. lowdb
---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-picture-upload',
+  };
+</script>
+
+<style>
+  /* CSS */
+</style>
